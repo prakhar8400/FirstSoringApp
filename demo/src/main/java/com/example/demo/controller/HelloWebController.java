47bbb65842ff7ca0c5f5bdbea1a2e1856ceb61ce
prakhar8400 <prakharm495@gmail.com>
2025-03-01 16:13:16 +0530

    Path Parameter

--- demo/src/main/java/com/example/demo/controller/HelloWebController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWebController.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 import org. springframework.stereotype.Controller;
 import org. springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWebController {
@@ -23,6 +20,11 @@ public class HelloWebController {
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "1";
+    }
+    // curl localhost:8080/hello/param/Naxayan -w "\n"
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 
 
